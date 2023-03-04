9126cfa5bbe42f3dbacc9421860f0ae546948481
asbonato <asbonato@gmail.com>
2023-03-04 09:54:49 -0300

    aula ecm516 04/03/23 - javascript

--- ecm516/js_intro/index.js
+++ ecm516/js_intro/index.js
@@ -1,73 +1,223 @@
-// // // //declarando constantes
-// // // const nome = 'José';
-// // // const idade = 27, outra_idade = 10;
-// // // //aspas simples e duplas tem o mesmo efeito
-// // // const sexo = "M";
-// // // const endereco = 'Rua K, 12';
-// // // console.log(nome, idade, outra_idade, sexo, endereco);  
+// // // // // // // // // //declarando constantes
+// // // // // // // // // const nome = 'José';
+// // // // // // // // // const idade = 27, outra_idade = 10;
+// // // // // // // // // //aspas simples e duplas tem o mesmo efeito
+// // // // // // // // // const sexo = "M";
+// // // // // // // // // const endereco = 'Rua K, 12';
+// // // // // // // // // console.log(nome, idade, outra_idade, sexo, endereco);  
 
-// // // //declarando variáveis
+// // // // // // // // // //declarando variáveis
 
-// // // //let: variável local, escopo de bloco
-// // // let a = 2;
-// // // let b = 'abc';
-// // // console.log(a, b);
-// // // b = 20;
-// // // console.log(a, b);
-// // // b = 20;
+// // // // // // // // // //let: variável local, escopo de bloco
+// // // // // // // // // let a = 2;
+// // // // // // // // // let b = 'abc';
+// // // // // // // // // console.log(a, b);
+// // // // // // // // // b = 20;
+// // // // // // // // // console.log(a, b);
+// // // // // // // // // b = 20;
 
-// // // /*var: variável global ou com escopo dentro da 
-// // // função onde foi criada*/
-// // // var c = 2 + 3
-// // // var d = "abcd"
-// // // console.log(c, d)
+// // // // // // // // // /*var: variável global ou com escopo dentro da 
+// // // // // // // // // função onde foi criada*/
+// // // // // // // // // var c = 2 + 3
+// // // // // // // // // var d = "abcd"
+// // // // // // // // // console.log(c, d)
 
-// // // console.log('antes da chave', x, a)
+// // // // // // // // // console.log('antes da chave', x, a)
 
-// // // if (a > 1){
-// // //     var x = 0
-// // //     a = 10
-// // //     console.log('dentro da chave', x, a)
+// // // // // // // // // if (a > 1){
+// // // // // // // // //     var x = 0
+// // // // // // // // //     a = 10
+// // // // // // // // //     console.log('dentro da chave', x, a)
+// // // // // // // // // }
+// // // // // // // // // console.log('fora da chave', x, a)
+
+// // // // // // // // var linguagem = 'Javascript'
+// // // // // // // // console.log('Aprendendo '+linguagem)
+// // // // // // // // //pode ser redeclarada
+// // // // // // // // var linguagem = 'Java'
+// // // // // // // // console.log('Aprendendo '+linguagem)
+
+// // // // // // // // var idade = 18;
+
+// // // // // // // // console.log(`Oi, ${nome}, tudo bem?`)
+
+// // // // // // // // if (idade >= 18) {
+// // // // // // // //     var nome = 'Joao'
+// // // // // // // //     console.log(`Parabéns, ${nome}. Você pode dirigir`)
+// // // // // // // // }
+// // // // // // // // console.log(`Até mais, ${nome}.`)
+
+// // // // // // // const n1 = 2
+// // // // // // // const n2 = '3'
+// // // // // // // //coerção implícita de n1, acontece concatenação
+// // // // // // // const n3 = n1 + n2
+// // // // // // // console.log(n3)
+// // // // // // //  //coerção explícita, soma acontece
+// // // // // // //  const n4 = n1 + Number(n2)
+// // // // // // //  console.log(n4)
+
+// // // // // // console.log(1 == 1) //true
+// // // // // // console.log(1 == "1") //true
+// // // // // // console.log(1 === 1) //true
+// // // // // // console.log(1 === "1") //false
+
+// // // // // // console.log(true == 1) //true
+// // // // // // console.log(true == 2) //false
+// // // // // // console.log(true == -1) //false
+// // // // // // console.log(false == 0) //true
+
+// // // // // // console.log(1 == [1]) // true
+// // // // // // console.log(null == null) //true
+// // // // // // console.log(null == undefined)//true
+// // // // // // console.log([] == false) //true
+// // // // // // console.log([] == []) // false
+
+// // // // // //declaração
+// // // // // v1 = []
+// // // // // v1[0] = 3.4
+// // // // // v1[10] = "abc"
+// // // // // console.log(v1.length)
+// // // // // console.log(v1)
+
+// // // // // //inicializando na criação
+// // // // // v2 = [2, "abc", true]
+// // // // // console.log(v2)
+
+// // // // // //iterando
+// // // // // for (let i = 0; i < v2.length; i++){
+// // // // //     console.log(v2[i])
+// // // // // }
+
+// // // // //métodos utilitário dos vetores
+
+// // // // const nomes = ["Ana Maria", "Antonio", "Rodrigo", "Alex", "Cristina"]
+
+// // // // //filter
+// // // // const apenasComA = nomes.filter(n => n.startsWith("A"))
+// // // // console.log(apenasComA)
+
+// // // // //map
+// // // // const res = nomes.map(nome => nome.charAt(0))
+// // // // console.log(res)
+
+// // // // //every
+// // // // const todosComecamComA = nomes.every(n => n.startsWith("A"))
+// // // // console.log(todosComecamComA)
+
+// // // // //reduce
+// // // // const valores = [1, 2, 3, 4]
+// // // // const soma = valores.reduce((ac, v) => ac + v)
+// // // // console.log(soma)
+
+// // // //criação de funções
+
+// // // function hello(){
+// // //     console.log('Oi')
 // // // }
-// // // console.log('fora da chave', x, a)
+// // // hello()
 
-// // var linguagem = 'Javascript'
-// // console.log('Aprendendo '+linguagem)
-// // //pode ser redeclarada
-// // var linguagem = 'Java'
-// // console.log('Aprendendo '+linguagem)
+// // // // cuidado, aqui redefinimos a função sem parâmetros
+// // // function hello(nome){
+// // //     console.log('Oi, '+nome)
+// // // }
+// // // hello('Pedro')
 
-// // var idade = 18;
+// // // function soma(a, b){
+// // //     return a + b
+// // // }
+// // // const res = soma(2, 3)
 
-// // console.log(`Oi, ${nome}, tudo bem?`)
+// // // console.log(res)
+// // // console.log(soma(2, 2))
+// // // console.log(hello('Silva'))
 
-// // if (idade >= 18) {
-// //     var nome = 'Joao'
-// //     console.log(`Parabéns, ${nome}. Você pode dirigir`)
+// // //funções anônimas
+
+// // const dobro = function (n) {
+// //     return n * 2
 // // }
-// // console.log(`Até mais, ${nome}.`)
+// // const res = dobro(4)
+// // console.log(res)
 
-// const n1 = 2
-// const n2 = '3'
-// //coerção implícita de n1, acontece concatenação
-// const n3 = n1 + n2
-// console.log(n3)
-//  //coerção explícita, soma acontece
-//  const n4 = n1 + Number(n2)
-//  console.log(n4)
+// // //valor default para parâmetro
+// // const triplo = function (n = 5) {
+// //     return 3 * n
+// // }
+// // console.log(triplo())
+// // console.log(triplo(10))
 
-console.log(1 == 1) //true
-console.log(1 == "1") //true
-console.log(1 === 1) //true
-console.log(1 === "1") //false
+// //arrow functions
+// const hello = () => console.log("Hello")
+// hello()
 
-console.log(true == 1) //true
-console.log(true == 2) //false
-console.log(true == -1) //false
-console.log(false == 0) //true
+// const dobro = valor => valor * 2
+// console.log(dobro(10))
 
-console.log(1 == [1]) // true
-console.log(null == null) //true
-console.log(null == undefined)//true
-console.log([] == false) //true
-console.log([] == []) // false
+// const triplo = valor => {
+//     return valor * 3
+// }
+// console.log(triplo(10))
+
+// const ehPar = n => {
+//     return n % 2 === 0
+// }
+// console.log(ehPar(10))
+
+//Closures
+
+/*uma função pode ser atribuída a uma variável*/
+// let umaFuncao = function () {
+//     console.log("Fui armazenada em uma variável")
+// }
+// umaFuncao()
+
+/* f recebe uma função como parâmetro e, por isso, é
+   uma função de alta ordem.
+   Por devolver uma função, g também é de alta ordem */
+
+function f (funcao){
+    //chamada da função
+    //note com a tipagem dinâmica tem seu preço
+    funcao()
+}
+
+function g() {
+    function outraFuncao(){
+        console.log("Fui criada por g")
+    }
+    return outraFuncao;
+}
+
+//f pode ser chamada assim
+//f(function() {
+//    console.log("Estou sendo passada para f")
+//})
+
+//e g pode ser chamada assim
+//const gResult = g()
+//gResult()
+
+//e assim também
+//g()()
+
+//outros testes
+/** f chama g, que somente devolve uma função; 
+ * nada é exibido */
+//f(g)
+
+/**f chama a função devolvida por g
+ * "Fui criada por g" é exibido
+ */
+//f(g())
+
+/**
+ * f tenta chamar o que a função criada por g
+ * devolve. Ela não devolve nada. Por isso,
+ * um erro acontece - somente em tempo de execução
+ */
+//f(g()())
+
+//o que acontece?
+f(1)
+
+
