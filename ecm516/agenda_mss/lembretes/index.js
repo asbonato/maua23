f5407e614883510d094a0d0bf5acf683c5ba9ee7
asbonato <asbonato@gmail.com>
2023-05-06 09:53:48 -0300

    ecm516 - aula 06/05 - mss - até seção 4.3.23

--- ecm516/agenda_mss/lembretes/index.js
+++ ecm516/agenda_mss/lembretes/index.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
+app.use(express.json())
+const lembretes = {}
+let contador = 0
 
 app.get("/lembretes", (req, res) => {
-  res.send("você chamou o get");
+  res.send(lembretes)
 });
 
 app.post("/lembretes", (req, res) => {
-  res.send("você chamou o post");
+  contador++
+  const { texto } = req.body 
+  lembretes[contador] = { 
+    contador, texto
+  }
+  res.status(201).send(lembretes[contador])
 });
 
 app.listen(4000, () => {
---
+++ ecm516/agenda_mss/observacoes/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
+const app = express()
+app.use(express.json())
+
+const observacoesPorLembreteId = {}
+     
+//:id é um placeholder
+//exemplo: /lembretes/123/observacoes
+
+app.post('/lembretes/:id/observacoes', (req, res) => {
+    const idObs = uuidv4()
+    const { texto } = req.body
+    //req.params dá acesso à lista de parâmetros da URL
+    const observacoesDoLembrete = 
+        observacoesPorLembreteId[req.params.id] || []
+    observacoesDoLembrete.push({
+        id: idObs,
+        texto
+    })
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete
+    res.status(201).send(observacoesDoLembrete)
+})
+
+app.get('/lembretes/:id/observacoes', (req, res) => {
+    res.send(observacoesPorLembreteId[req.params.id] || [])
+})
+
+// app.get('/lembretes/observacoes', (req, res) => {
+//     res.send(observacoesPorLembreteId)
+// })
+
+app.listen(5000, (() => {
+    console.log('Observações. Porta 5000.')
+}))
