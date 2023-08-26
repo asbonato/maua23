d757bd788ba96e3b1ca08a5b1c76ec0d46adf550
asbonato <asbonato@gmail.com>
2023-08-26 12:52:37 -0300

    ecm252 aula 26/8 dart colecoes até pg 6 - exercício soma

---
+++ ecm252/projetos_dart/colecoes/bin/colecoes.dart
@@ -0,0 +1,7 @@
+void main(List<String> arguments) {
+  var soma = 0;
+  for(final valor in arguments){
+    soma += int.parse(valor);
+  }
+  print(soma);
+}
---
+++ ecm252/projetos_dart/colecoes/test/colecoes_test.dart
@@ -0,0 +1,8 @@
+import 'package:colecoes/colecoes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
