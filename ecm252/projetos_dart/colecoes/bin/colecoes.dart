ab4f15507df051c08b1732303c87700471bf715b
asbonato <asbonato@gmail.com>
2023-09-16 10:32:48 -0300

    ecm252 - aula 16/9 - dart - apostila 3 terminada

--- ecm252/projetos_dart/colecoes/bin/colecoes.dart
+++ ecm252/projetos_dart/colecoes/bin/colecoes.dart
@@ -1,22 +1,65 @@
+import 'dart:io';
 void main(List<String> arguments) {
-  var pessoa = <String, dynamic>{'nome': 'Pedro', 'idade': 22};
+// // //   // // //coleções de coleções
+// // //   // // var filmes = <Map<String, dynamic>>[];
+// // //   // // print(filmes.runtimeType);
 
-  // var chaves = pessoa.keys;
-  // print(chaves.runtimeType);
+// // //   // // print('Titulo?');
+// // //   // // String? titulo = stdin.readLineSync();
+// // //   // // print('Gênero?');
+// // //   // // String? genero = stdin.readLineSync();
+// // //   // // var notas = [5, 5];
+// // //   // // filmes.add({'titulo':titulo, 'genero':genero, 'notas':notas});
+// // //   // // print(filmes);
+// // //   // //
+// // //   // // collection-if
+// // //   // var idadePedro = 17;
+// // //   // var idadeCristina = 19;
+// // //   // var maioresDeIdade = [
+// // //   //   'Ana', 
+// // //   //   'João',
+// // //   //   if (idadePedro >= 18) 'Pedro',
+// // //   //   if (idadeCristina >= 18) 'Cristina'
+// // //   // ];
+// // //   // print(maioresDeIdade);
+// // //   //
+// // //   // collection-for
+// // //   var nomes1 = ['Ana', 'Pedro'];
+// // //   var nomes2 = [
+// // //     'Cristina',
+// // //     for (var nome in nomes1)
+// // //       nome
+// // //   ];
+// // //   print(nomes2);
+// // //
+// // // operador spread
+// //   var nomes1 = ['Ana', 'Pedro'];
+// //   var nomes2 = [
+// //     'Cristina',
+// //     ...nomes1
+// //   ];
+// //   print(nomes2);
+// var nomes = ['Ana', 'Pedro'];
+// var copia = nomes;
+// print(copia);
+// nomes[0] = 'Maria';
+// print(copia);
+//
+// cópia com collections-for e com spread
+  var nomes = ['Ana', 'Pedro'];
 
-  // for (final propriedade in chaves) {
-  //   print(propriedade);
-  //   print(pessoa[propriedade]);
-  // }
-  // var valores = pessoa.values;
-  // for (final valor in valores) {
-  //   print(valor);
-  // }
-  var entries = pessoa.entries;
-  print(entries.runtimeType);
-  for(final entry in pessoa.entries){
-    print(entry);
-    print(entry.key);
-    print(entry.value);
-  }
+  var copiaComCollectionFor = [
+    for(var nome in nomes)
+      nome
+  ];
+  var copiaComOperadorSpread = [
+    ...nomes
+  ];
+  print(nomes);
+  print(copiaComCollectionFor);
+  print(copiaComOperadorSpread);
+  nomes[0] = 'Maria';
+  print(nomes);
+  print(copiaComCollectionFor);
+  print(copiaComOperadorSpread);
 }
